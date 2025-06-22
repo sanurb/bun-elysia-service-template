a4e44874a77aa571b8e6417506646726e8f83f6d
David Urbano <60886336+sanurb@users.noreply.github.com>
2025-06-22 08:54:03 -0500

    feat: add domain_error.ts

---
+++ src/shared/domain/domain_error.ts
@@ -0,0 +1,20 @@
+export abstract class DomainError extends Error {
+  abstract type: string;
+  abstract message: string;
+
+  toPrimitives(): {
+    type: string;
+    description: string;
+    data: Record<string, unknown>;
+  } {
+    const props = Object.entries(this).filter(
+      ([key, _]) => key !== 'type' && key !== 'message'
+    );
+
+    return {
+      type: this.type,
+      description: this.message,
+      data: Object.fromEntries(props),
+    };
+  }
+}
